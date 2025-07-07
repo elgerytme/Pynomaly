854f84bedc388bca6f93fdd07526af405d1e8a27
elgerytme <elgerytme@gmail.com>
2025-07-07 19:23:38 -0400

    bump: Update version to 0.1.dev755+gf527c12

--- src/pynomaly/_version.py
+++ src/pynomaly/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev754+g75c75a6.d20250707'
-__version_tuple__ = version_tuple = (0, 1, 'dev754', 'g75c75a6.d20250707')
+__version__ = version = '0.1.dev755+gf527c12'
+__version_tuple__ = version_tuple = (0, 1, 'dev755', 'gf527c12')
